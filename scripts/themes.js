3b6c131f0a236f0263ca35ed0c92a321eccc0f9b
Thainno Santana <thainnosv@gmail.com>
2022-11-16 09:25:24 -0300

    factorization themes js

--- scripts/themes.js
+++ scripts/themes.js
@@ -1,25 +1,11 @@
-let imgTheme = document.querySelector('#img-theme')
+const imgTheme = document.getElementById('img-theme')
+const colors = ['Purple', 'Yellow', 'Blue', 'Green']
 
-let themePurple = document.querySelector('#themePurple').addEventListener('click', () => {
+colors.forEach((color) => {
+  document.getElementById(`theme${color}`).addEventListener('click', () => {
     document.body.removeAttribute('class')
-    document.body.classList.toggle('purple')
-    imgTheme.setAttribute('src', '../images-geometric/purple.png')
-})
-
-let themeYellow = document.querySelector('#themeYellow').addEventListener('click', () => {
-    document.body.removeAttribute('class')
-    document.body.classList.toggle('yellow')
-    imgTheme.setAttribute('src', '../images-geometric/yellow.png')
-})
-
-let themeBlue = document.querySelector('#themeBlue').addEventListener('click', () => {
-    document.body.removeAttribute('class')
-    document.body.classList.toggle('blue')
-    imgTheme.setAttribute('src', '../images-geometric/blue.png')
-})
-
-let themeGreen = document.querySelector('#themeGreen').addEventListener('click', () => {
-    document.body.removeAttribute('class')
-    document.body.classList.toggle('green')
-    imgTheme.setAttribute('src', '../images-geometric/green.png')
+    const tlc = color.toLowerCase()
+    document.body.classList.toggle(tlc)
+    imgTheme.setAttribute('src', `/images-geometric/${tlc}.png`)
+  })
 })
