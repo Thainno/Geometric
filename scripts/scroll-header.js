34db739b1540c532a1ef80bcd20437a38abea795
Thainno Santana <thainnosv@gmail.com>
2022-11-21 21:26:31 -0300

    create cards section metaverses

--- scripts/scroll-header.js
+++ scripts/scroll-header.js
@@ -1,11 +1,11 @@
 const scrollHeader = document.querySelector('header')
 const logoHeader = document.querySelector('#logo')
 
-
 function activeScroll() {
     if (scrollHeader.classList.toggle('scroll', scrollY > 90)) {
         logoHeader.setAttribute('src', '../images/logo/purple-logo.svg')
         scrollHeader.style.cssText = 'border-color: #070568'
+
     } else {
         logoHeader.setAttribute('src', '../images/logo/white-logo.svg')
         scrollHeader.style.cssText = 'border-color: #ffffff'
