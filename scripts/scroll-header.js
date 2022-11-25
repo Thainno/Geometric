ca99dfc8efcc6720eab4aa10af4c16adbddbb013
Thainno Santana <thainnosv@gmail.com>
2022-11-24 21:45:19 -0300

    hover buttons

--- scripts/scroll-header.js
+++ scripts/scroll-header.js
@@ -5,7 +5,6 @@ function activeScroll() {
     if (scrollHeader.classList.toggle('scroll', scrollY > 90)) {
         logoHeader.setAttribute('src', '../images/logo/purple-logo.svg')
         scrollHeader.style.cssText = 'border-color: #070568'
-
     } else {
         logoHeader.setAttribute('src', '../images/logo/white-logo.svg')
         scrollHeader.style.cssText = 'border-color: #ffffff'
