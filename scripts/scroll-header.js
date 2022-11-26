5a2656174da3b140a5fb35c8be009f519ceecef8
Thainno Santana <thainnosv@gmail.com>
2022-11-25 21:04:24 -0300

    delete section #metaverse

--- scripts/scroll-header.js
+++ scripts/scroll-header.js
@@ -2,13 +2,7 @@ const scrollHeader = document.querySelector('header')
 const logoHeader = document.querySelector('#logo')
 
 function activeScroll() {
-    if (scrollHeader.classList.toggle('scroll', scrollY > 90)) {
-        logoHeader.setAttribute('src', '../images/logo/purple-logo.svg')
-        scrollHeader.style.cssText = 'border-color: #070568'
-    } else {
-        logoHeader.setAttribute('src', '../images/logo/white-logo.svg')
-        scrollHeader.style.cssText = 'border-color: #ffffff'
-    }
+   scrollHeader.classList.toggle('scroll', scrollY > 450)
 }
 
 window.addEventListener('scroll', activeScroll)
