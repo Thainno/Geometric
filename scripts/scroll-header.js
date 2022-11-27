6bff8c770cc7440b17c0a3be57a3c1df467fdae3
Thainno Santana <thainnosv@gmail.com>
2022-11-27 12:56:07 -0300

    initial section metaverse test

--- scripts/scroll-header.js
+++ scripts/scroll-header.js
@@ -1,5 +1,4 @@
 const scrollHeader = document.querySelector('header')
-const logoHeader = document.querySelector('#logo')
 
 function activeScroll() {
    scrollHeader.classList.toggle('scroll', scrollY > 450)
